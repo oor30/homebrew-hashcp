b96090dee6404c72d1968417690e1a01089b2386
oor30 <kazuki.w.0920@gmail.com>
2024-04-21 05:19:51 +0900

    initial commit

---
+++ hashcp.rb
@@ -0,0 +1,16 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Hashcp < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/oor30/hashFilename/releases/download/1.0/hashcp-v1.0.tar.gz"
+  sha256 "1a26875b47a35ce3cf95d68c994e0130a7305ba2d09a30159198b8e86f3f05a2"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    bin.install 'hashcp'
+  end
+end
