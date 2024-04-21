2680b60e4bc96387a48bb0c473f812a92a05ab7a
oor30 <kazuki.w.0920@gmail.com>
2024-04-21 23:37:51 +0900

    [fix]

--- Formula/hashcp.rb
+++ Formula/hashcp.rb
@@ -2,7 +2,7 @@ class Hashcp < Formula
   include Language::Python::Virtualenv
   desc "copy files in directory, and rename the files hash value."
   homepage "https://github.com/oor30/homebrew-hashcp"
-  url "https://github.com/oor30/hashFilename/releases/download/1.0/hashcp_package-0.1.0.tar.gz"
+  url "https://github.com/oor30/hashFilename/releases/download/1.0/hashcp-0.1.0.tar.gz"
   sha256 "f3dd323fb13ee2a2362d965fa965d849f70a187047ce36c7824c8c0af6d8eda7"
   license ""
 
