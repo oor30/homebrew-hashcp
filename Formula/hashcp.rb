889fe47c446b7411adec3a7d677ada6b44533c98
oor30 <kazuki.w.0920@gmail.com>
2024-04-21 17:57:13 +0900

    [fix]

--- Formula/hashcp.rb
+++ Formula/hashcp.rb
@@ -6,7 +6,7 @@ class Hashcp < Formula
   desc ""
   homepage ""
   url "https://github.com/oor30/hashFilename/releases/download/1.0/hashcp_package-0.1.0.tar.gz"
-  sha256 "1a26875b47a35ce3cf95d68c994e0130a7305ba2d09a30159198b8e86f3f05a2"
+  sha256 "f3dd323fb13ee2a2362d965fa965d849f70a187047ce36c7824c8c0af6d8eda7"
   license ""
 
   # depends_on "cmake" => :build
